82f49afcd37aec943bac87e2e0c41930e28cb1b2
league <league@192.168.1.200>
2026-01-05 17:02:06 -0800

    debugging; do the smmme thing with ints as you did with strings

--- src/_07_Meeting_Scheduler/MeetingScheduler.java
+++ src/_07_Meeting_Scheduler/MeetingScheduler.java
@@ -4,36 +4,47 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MeetingScheduler  {
-    /*
-     * Your task is to code a method to find a meeting time for two people
-     * given their schedules.
-     * 
-     * Code the method below so that it returns a Schedule object that contains
-     * all the times during the week that are in BOTH people's schedules. The
-     * Schedule class is included in this package.
-     * 
-     * Example:
-     * person1 availability - Monday at 9, Tuesday at 14, and Friday 10
-     * person2 availability - Tuesday at 14, Friday 8, and Monday at 9
-     * The returned HashMap should contain: Tuesday 14 and Monday 9
-     * 
-     * The returned Schedule object represents the times both people are
-     * available for a meeting.
-     * 
-     * Time availability is always at the top of the hour, so 9:30 is not valid
-     * Time availability always represents 1 hour
-     * Assume both schedules are in the same time zones
-     */
-    public static Schedule getMutualAvailability(Schedule person1, Schedule person2) {
-        Schedule sch = new Schedule();
-        
-        for(String s : person1.weeklyAvailability.keySet()){
-        	 if(person1.weeklyAvailability.get(s) == person2.weeklyAvailability.get(s)) {
-        		 sch.addAvailability(s, person1.weeklyAvailability.get(s));
-        	 }
-        }
-        
-		
-        
-    }
+	/*
+	 * Your task is to code a method to find a meeting time for two people
+	 * given their schedules.
+	 * 
+	 * Code the method below so that it returns a Schedule object that contains
+	 * all the times during the week that are in BOTH people's schedules. The
+	 * Schedule class is included in this package.
+	 * 
+	 * Example:
+	 * person1 availability - Monday at 9, Tuesday at 14, and Friday 10
+	 * person2 availability - Tuesday at 14, Friday 8, and Monday at 9
+	 * The returned HashMap should contain: Tuesday 14 and Monday 9
+	 * 
+	 * The returned Schedule object represents the times both people are
+	 * available for a meeting.
+	 * 
+	 * Time availability is always at the top of the hour, so 9:30 is not valid
+	 * Time availability always represents 1 hour
+	 * Assume both schedules are in the same time zones
+	 */
+	public static Schedule getMutualAvailability(Schedule person1, Schedule person2) {
+		Schedule sch = new Schedule();
+
+		for(String s : person1.weeklyAvailability.keySet()){
+			for(String s2 : person2.weeklyAvailability.keySet())
+			 if(s.equals(s2)) {
+				 
+				for(int i: person1.weeklyAvailability.get(s)) {
+					System.out.println(s);
+					System.out.println(i);
+					sch.addAvailability(s2, i);
+				}
+
+
+			}
+		}
+		return sch;
+
+
+
+	}
 }
+
+
